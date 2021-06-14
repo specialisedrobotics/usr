674016a529f3795ff93830816b947a106d7d8c2a
specialisedrobotics <82479768+specialisedrobotics@users.noreply.github.com>
2021-06-14 19:00:08 +0100

    Add files via upload

---
+++ electronics/modules.py
@@ -0,0 +1,13 @@
+"""Control modules
+
+Communication module with RF
+Opticall communication using light
+Behavioral communication
+
+
+"""
+
+communication = 'radio frequency'
+optical_communication = 'optically coded information'
+transceivers = 'transmitting data'
+
