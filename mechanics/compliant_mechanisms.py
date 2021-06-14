b1ad7b23c715afec8ab4e9e9fd93151b8aaf418c
specialisedrobotics <82479768+specialisedrobotics@users.noreply.github.com>
2021-06-14 18:57:27 +0100

    Add files via upload

---
+++ mechanics/compliant_mechanisms.py
@@ -0,0 +1,15 @@
+"""Compliant mechanisms
+
+Reduce the mechanical joints
+Reduce the maintenance
+Lighter and a more robust structure
+
+"""
+
+# Identify the moving parts
+
+
+# Calculate the relevant compliant mechanism
+
+
+# Integrate its solution to the final part
